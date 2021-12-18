3f30981fe3e7d97cce45b31fda7a0825c619ed20
Daniel <daniel.f.bourke@gmail.com>
2021-12-18 15:26:05 +0800

    finished part 2 I think

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -30,7 +30,11 @@ data.forEach((sighting) => {
 var filter_table_button = d3.select("#filter-btn");
 
 // Filter_Input_Element
-var Filter_Input_Element = d3.select("#datetime");
+var date_Filter_Input_Element = d3.select("#datetime");
+var city_Filter_Input_Element = d3.select("#city");
+var state_Filter_Input_Element = d3.select("#state");
+var country_Filter_Input_Element = d3.select("#country");
+var shape_Filter_Input_Element = d3.select("#shape");
 
 
 
@@ -44,19 +48,56 @@ filter_table_button.on("click", (event) => {
     tbody.html("");
 
     // get the value of the filter input box 
-    var Filter_Input_Value = Filter_Input_Element.property("value");
+    var date_Filter_Input_Value = date_Filter_Input_Element.property("value");
+    var city_Filter_Input_Value = city_Filter_Input_Element.property("value");
+    var state_Filter_Input_Value = state_Filter_Input_Element.property("value");
+    var country_Filter_Input_Value = country_Filter_Input_Element.property("value");
+    var shape_Filter_Input_Value = shape_Filter_Input_Element.property("value");
+
+
+    if (date_Filter_Input_Value == "") {
+        var Data_That_Matches_date_Filter = tableData
+    }
+    else {
+        Data_That_Matches_date_Filter = tableData.filter(tableData => tableData.datetime === date_Filter_Input_Value);
+    };
+    if (city_Filter_Input_Value == "") {
+        var Data_That_Matches_city_Filter = Data_That_Matches_date_Filter
+    }
+    else {
+        Data_That_Matches_city_Filter = Data_That_Matches_date_Filter.filter(Data_That_Matches_date_Filter => Data_That_Matches_date_Filter.city === city_Filter_Input_Value);
+    };
+    if (state_Filter_Input_Value == "") {
+        var Data_That_Matches_state_Filter = Data_That_Matches_city_Filter
+    }
+    else {
+        Data_That_Matches_state_Filter = Data_That_Matches_city_Filter.filter(Data_That_Matches_city_Filter => Data_That_Matches_city_Filter.state === state_Filter_Input_Value);
+    };
+    if (country_Filter_Input_Value == "") {
+        var Data_That_Matches_country_Filter = Data_That_Matches_state_Filter
+    }
+    else {
+        Data_That_Matches_country_Filter = Data_That_Matches_state_Filter.filter(Data_That_Matches_state_Filter => Data_That_Matches_state_Filter.country === country_Filter_Input_Value);
+    };
+    if (shape_Filter_Input_Value == "") {
+        var Data_That_Matches_shape_Filter = Data_That_Matches_country_Filter
+    }
+    else {
+        Data_That_Matches_shape_Filter = Data_That_Matches_country_Filter.filter(Data_That_Matches_country_Filter => Data_That_Matches_country_Filter.shape === shape_Filter_Input_Value);
+    };
+
 
     // Data_That_Matches_Filter is data where the datetime column value matches the filter input value
-    var Data_That_Matches_Filter = tableData.filter(tableData => tableData.datetime === Filter_Input_Value);
+    // var Data_That_Matches_Filter = tableData.filter(tableData => tableData.datetime === date_Filter_Input_Value);
 
     // Account for the case in which no sightings occured on a chosen date
-    if (Data_That_Matches_Filter.length == 0) {
+    if (Data_That_Matches_shape_Filter.length == 0) {
         // add a new row, and a cell/td saying there was no sightings
         tbody.append("tr").append("td").text("there were no sightings on this date")
     }
     else {
         // for each Sighting that matches the filter
-        Data_That_Matches_Filter.forEach((Sighting_That_Matches_Filter) => {
+        Data_That_Matches_shape_Filter.forEach((Sighting_That_Matches_Filter) => {
             // create a new row in the html table
             var row = tbody.append("tr");
             // for each column of the sighting
